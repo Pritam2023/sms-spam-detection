b6b0de100423555ffa7f00eb6000ff76d08eb3b2
Pritam Chatterjee <74919879+Pritam2023@users.noreply.github.com>
2025-06-15 00:26:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,14 +41,16 @@ st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 if st.button('Predict'):
-    # 1. Preprocess the input text
-    transformed_sms=transform_text(input_sms)
-    # 2. Vectorization
-    vector_input = tfidf.transform([transformed_sms])
-    # 3. Predict
-    result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
+    if(len(input_sms)==0):
+        st.header("Please enter the sms or mail")
+        # 1. Preprocess the input text
+        transformed_sms=transform_text(input_sms)
+        # 2. Vectorization
+        vector_input = tfidf.transform([transformed_sms])
+        # 3. Predict
+        result = model.predict(vector_input)[0]
+        # 4. Display
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
